f3c9a6aa0936af5c49c4463f0cc45aa43b0aee0f
nhinojos <noah.hinojos@gmail.com>
2022-02-27 22:00:53 -0800

    Altered Excel Workbook Generation. Defined generateWorkbook function. New workbook includes multiple worksheets & labeled data cells. Deleted previous excel document.

--- Amazon-to-Crypto.py
+++ Amazon-to-Crypto.py
@@ -1,31 +1,47 @@
-## Necessary Python Libararies
-# Requesting Cryptocurrency Data form Coinbase
+## Necessary Python Libararies.
+# Requesting Cryptocurrency Data form Coinbase.
 import requests
-# Manipulating Excel Documents
-import openpyxl
+# Manipulating Excel Documents.
+from openpyxl import Workbook
 import time
-# Email Delivery
+# Email Delivery.
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
-from email import encoders
 
 
+## Initial Prompt to either use same workbook or generate new workbook.
 
-## Webscraping Amazon Price using
+
+## Webscraping Amazon Price using.
 
 
-# Requesting cryptocurrency prices form CoinBase. 
+## Tracking Information in Excel Document.
+#Generates new Excel workbook.
+def generateWorkbook(filename,cryptolist):
+    # Creating workbook object and data worksheet sub-object.
+    wb=Workbook()
+    ws_data=wb.active
+    ws_data.title = "Data"
+    # Labeling headers.
+    ws_data['A1']="Currency"
+    ws_data['B1']="Date" # Should range B1:n1, where B to n are the dates recorded.
+    # Labeling cryptos of choice in the left-most column.
+    for i,crypto in enumerate(cryptolist):
+        ws_data['A'+str(i+2)]=crypto
+    #Creating separet worksheet analyze data
+    ws_analysis=wb.create_sheet("Analysis")
+    #Saving workbook
+    wb.save(filename+'.xlsx')
+    return wb
+
+# Requesting current cryptocurrency prices from CoinBase. 
 r=requests.get('https://api.coinbase.com/v2/exchange-rates').json()
-crypto='BTC'
-#print(r['data']['rates'][crypto])
-
-
-## Tracking Information in Excel Document
-#Excel Manipulation
-xbook=openpyxl.load_workbook('Amazon-Crypto-Data.xlsx')
+cryptolist=['BTC','ETH','LTC'] # Can be any list of crypto.
 current_hour_minute=time.strftime("%H:%M",time.localtime())
+"""print(r['data']['rates']['BTC])"""
+
+#Generating test workbook
+wb=generateWorkbook('testworkbook',cryptolist)
+
 
 
 ## Email Delivery.
@@ -35,7 +51,7 @@ password='hinojos123'
 subject='This is a test written by me. Thanks for reading!'
 email_receiver='noah.hinojos@gmail.com'
 
-# SMTP connection and email delivery
+# SMTP connection and email delivery.
 smtp_obj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 smtp_obj.login(email_sender, password)
 smtp_obj.sendmail(email_sender,email_receiver,subject)
