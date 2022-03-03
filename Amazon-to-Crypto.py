070481b28369b1a9d45aaad29c7055ef53042af0
nhinojos <noah.hinojos@gmail.com>
2022-03-03 15:36:44 -0800

    Reconfigured Email Delivery. Now simpler through yagmail library. Can easily edit subject and add attachments.

--- Amazon-to-Crypto.py
+++ Amazon-to-Crypto.py
@@ -1,20 +1,24 @@
-## Necessary Python Libararies.
-# Requesting Cryptocurrency Data form Coinbase.
-import requests
-# Manipulating Excel Documents.
-from openpyxl import Workbook
-import os.path 
-import time
-# Email Delivery.
-import smtplib
+## Necessary Libararies.
+import requests 
+from openpyxl import Workbook # Manipulating Excel Spreadsheet. Documentation: https://openpyxl.readthedocs.io/en/stable/
+import os.path
+import yagmail #Simplified Gmail Delivery. Documentation: https://github.com/kootenpv/yagmail
 
+##Function Creation
+#Delivering with Gmail easily using yagmail
+def emailMessage(title,content,receiver,sender):
+    """ 
+    If you haven't done so already, register your email 
+    and password using python's secure  library 
+    'keyring.' Then, run the following command:
+    yagmail.register(gmailusername,mygmailpassword)
+    
+    You may also need to confirm 3rd party access to 
+    your Gmail account in order to run this program. 
+    """
+    yagmail.SMTP(sender).send(receiver,title,content)
+    return
 
-
-## Webscraping Amazon Price using.
-
-
-
-## Tracking Information in Excel Document.
 # Generates new Excel workbook.
 def generateWorkbook(crypto_list):
     # Creating workbook object and data worksheet sub-object.
@@ -23,10 +27,9 @@ def generateWorkbook(crypto_list):
     ws_data.title = "Data"
     ws_data['A1']="Currency"
     ws_data['B1']="Date" # Should range B1:n1, where B to n are the dates recorded.
-    
     for i,crypto in enumerate(crypto_list):
         ws_data['A'+str(i+2)]=crypto
-    
+
     wb.create_sheet("Analysis")
     wb.save('product_history.xlsx')
     return wb
@@ -34,11 +37,24 @@ def generateWorkbook(crypto_list):
 
 ## Intializes new workbook if there is not one already in the directory. 
 if not os.path.exists('product_history.xlsx'):
-    print('No workbook found in directory.')
+    print("No workbook titled 'product_history.xlsx' found in directory.")
+    while True:
+        response=input('Would you like to generate a new workbook?(y/n)')
+        response.lower()
+        response.strip()
+        if response in {'y','n','yes','no'}:
+            if response in {'n','no'}:
+                print("Please ensure a valid workbook titled 'product_history.xlsx' exists before restarting.")
+                quit()
+            break
+        else:
+            print('That is not a valid response.')
     print('Initializing workbook generation.')
+
     product_link=input('Please provide a link to the amazon product:')
     
-    print('Now, here are a list of cryptocurrencies available to track')
+    #Requesting Coinbase Data
+    print('Here are the list of cryptocurrencies available to track')
     r=requests.get('https://api.coinbase.com/v2/exchange-rates').json()
     crypto_options=r['data']['rates'].keys()
     crypto_string=''
@@ -47,7 +63,8 @@ if not os.path.exists('product_history.xlsx'):
         if i%16==0:
             print(crypto_string)
             crypto_string=''
-    
+
+    #Tracking proper currencies. 
     print('Please type the abbreviated cryptocurrencies you would like to track, separated by commas:')
     crypto_list=input()
     crypto_list.upper()
@@ -55,30 +72,7 @@ if not os.path.exists('product_history.xlsx'):
     crypto_list.split(',')
     generateWorkbook(crypto_list)
 
-
-## Requesting current cryptocurrency prices from CoinBase. 
-r=requests.get('https://api.coinbase.com/v2/exchange-rates').json()
-cryptolist=['BTC','ETH','LTC'] # Can be any list of crypto.
-current_hour_minute=time.strftime("%H:%M",time.localtime())
-"""print(r['data']['rates']['BTC])"""
-
-
-#Generating test workbook
-wb=generateWorkbook('product_history',cryptolist)
+#Tabulates data to excel workbook. 
 
 
 
-## Email Delivery.
-# Defining relevant variables.
-email_sender="kitten.in.superposition@gmail.com"
-password='hinojos123'
-subject='This is a test written by me. Thanks for reading!'
-email_receiver='noah.hinojos@gmail.com'
-
-# SMTP connection and email delivery.
-smtp_obj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-smtp_obj.login(email_sender, password)
-#smtp_obj.sendmail(email_sender,email_receiver,subject)
-
-# Ending SMTP Connection.
-smtp_obj.quit()
