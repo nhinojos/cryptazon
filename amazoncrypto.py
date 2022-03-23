0efb9c552cfc9711c6dec91a0c1cdd5800311c66
nhinojos <noah.hinojos@gmail.com>
2022-03-22 17:32:15 -0700

    Debugeed Price Sraping. Can now successfully append threshold data to excel document.

--- amazoncrypto.py
+++ amazoncrypto.py
@@ -40,6 +40,7 @@ class ProductTracker:
         # self.recipient is the email being sent notifcations.
         # self.password is the sender email's password.
         # self.driver_location is the file location of your chrome driver. 
+            # The default is the C:\ drive. 
     def __init__(
             self, filename, link, cryptocurrencies,
             email_recipient, email_sender,  
@@ -53,10 +54,13 @@ class ProductTracker:
         self.email_recipient=email_recipient
         self.driver_location=driver_location
 
-        # Generate new Excel workbook.
+        ## Generate new Excel workbook.
+        # Creates two new sheets: "Data" and "Analysis"
         workbook=Workbook() 
-        workbook.active.title="Data" 
+        workbook.active.title="Data"
+        workbook.create_sheet("Analysis") 
         ws_data=workbook["Data"]
+        # Appends currency types in column headers
         ws_data['B1'].value='USD'
         i=0
         for col in ws_data.iter_cols(min_row=1,max_row=1,min_col=3,
@@ -64,33 +68,45 @@ class ProductTracker:
             for cell in col:
                 cell.value=self.cryptocurrencies[i]
                 i+=1
+        # Appends threshold rates under column headers
         ws_data['A2']="Threshold"
         if threshold==None:
             threshold=self.get_price()
-        ws_data['A3']=threshold
+        self.threshold=float(threshold)
+        ws_data['B2']=threshold
+        crypto_rates=[]
+        for crypto in cryptocurrencies:
+            crypto_rates.append(float(currency_data()[crypto])*self.threshold)
         
-        workbook.create_sheet("Analysis")
+        for row in ws_data.iter_rows(min_row=2,max_row=2,min_col=3,
+                                    max_col=2+len(cryptocurrencies)):
+            for i,cell in enumerate(row):
+                cell.value=crypto_rates[i]
+            
         self.workbook=workbook
 
-        # Establishes emails and password, if relevant
+        ## Registers delivering email and password,
+        ## if not done so already.
         if password!=None:
             yagmail.register(email_sender,password)
+        
         return 
 
 
-    ## Notifies user through email.
+    ## Delivers email notification to user.
     def email_notification(self, title, content):
         date=time.strftime("%d %b %y, %H:%M%p",time.localtime())
         title="Amazon-Crypto Tracker:"+self.filename+date
         yagmail.SMTP(self.sender).send(self.recipient,title,content)
         return None
 
-    # Establishes threshold. 
+    # Edits threshold for notification. 
     def new_threshold(self,threshold):
         self.threshold=threshold
         return
 
     ## Saves excel workbook to directory. 
+        # May be combined with below function save_object. 
     def save_workbook(self): 
         self.workbook.save(self.filename+'.xlsx')
         return
@@ -100,28 +116,19 @@ class ProductTracker:
         pickle.dump(self,open(self.filename+'.pickle','wb'))
         return
 
-    ## Appends price data to excel workbook
+    ## Retrieves price through webscraping.
     def get_price(self,in_usd=True):
-        ## Webscrapes price through Selenium Chrome driver. 
+        # Executes Selenium Chrome driver. 
         driver=webdriver.Chrome(self.driver_location) 
         driver.get(self.link)
-        
+        # Writes entire HTML to a document
         with open("product_page.html", "w",encoding='utf-8') as f:
             f.write(driver.page_source)
+        # Retrieves price from HTML document. 
         with open("product_page.html", "rb") as f:
             soup=BeautifulSoup(f,'lxml')
-        price_usd =soup.find('span',class_='a-offscreen').text.replace('$','')
-        
-        #Reutrns in terms of USD
-        if in_usd:
-            return price_usd
-        # Returns in terms of self.crypticurrencies
-        else:
-            # Converts price to cryptocurrency
-            price_crypto=[]
-            for crypto in self.cryptocurrencies:
-                price_crypto.append(price_usd*currency_data[crypto])
-            return price_crypto
+        price_usd=soup.find('span',class_='a-offscreen').text.replace('$','')
+        return float(price_usd)
             
 
     # Adds prices to workbook
