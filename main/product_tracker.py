3d763feb3395ab5f89685b9da7affbc448067f72
nhinojos <noah.hinojos@gmail.com>
2022-06-05 14:42:14 -0700

    Email functionality discontinued.
    Google's ToS completely alters how YagMail functions, unfortunately.

--- main/product_tracker.py
+++ main/product_tracker.py
@@ -13,65 +13,63 @@ class ProductTracker:
     def __init__(
         self,
         product_link: str,
-        email_recipient: str,
-        email_sender: str,
         password: str = None,
         thresholds: dict = None,
+        dataframe: pd.DataFrame = None,
         filename: str = 'product',
         driver_path: str = "C:\\chromedriver.exe",
-        csv_path: str = None
     ) -> None:
         
+        self.driver_path = driver_path
+        self.name = filename
         self.product_link = product_link
         self._validate_connection()
-        self.email_recipient = email_recipient
-        self.email_sender = email_sender
         
-        # Registers password if not so done already.
-        if password is not None:
-            yagmail.register(email_sender, password)
-        self.driver_path = driver_path
-        self.filename = filename
-
         # Assigns empty threshold values to corresponding product price
         # in terms of currency exchange rate.
         self.thresholds = thresholds
-        self._to_id()
+        self._cryptokeys_to_id()
         update_data = True
         for key,val in zip(self.thresholds.keys(), self.thresholds.values()):
             if val == None:
                 if key == "usd":
-                    self.thresholds[key] = self.scrape_price()
+                    self.thresholds[key] = self.scrape_price_usd()
                     update_data = False
                 
                 else:
                     self.thresholds[key] = (
                         self.thresholds["usd"] 
-                        / self.coins_per_usd(key)
+                        / self.coin_per_usd(key)
                     )
         
-        if csv_path is None:
+        if dataframe is None:
             self.df = pd.DataFrame(columns=["date",*self.thresholds.keys()])
             if update_data:
-                self.update_df()
-        
+                self.update_data()
         else:
-            self.df = pd.read_csv(csv_path)
+            self.df = dataframe
         
-        
-    @staticmethod
-    def present_date(include_time: bool = False) -> str:
+
+    def scrape_price_usd(self) -> float:
         """
-        Returns the present date is DD-MM-YYYY Format. May include 
-        24-hour time as well, formatted as HH:MM.
+        Scrapes Amazon's productpage for current USD pricepoint.
         """
-        date = strftime("%d-%m-%Y", localtime())
-        if include_time:
-            date += " " + strftime("%H:%M", localtime())
-        return date
+        driver = webdriver.Chrome(self.driver_path)
+        driver.get(self.product_link)
+        with open( 
+            "data\\" + self.name + "_page.html", 
+            "w", 
+            encoding='utf-8'
+        ) as f:
+            f.write(driver.page_source)
+        with open("data\\"+ self.name + "_page.html","rb") as f:
+            soup=BeautifulSoup(f, 'lxml')
+        return float(
+            soup.find("span", class_="a-offscreen").text.replace("$", "")
+        )
 
 
-    def currency_history(id: str, date: str, display: bool = False) -> dict:
+    def coin_is_high(id: str, date: str, display: bool = False) -> dict:
         """
         API call for historical cryptocurrency rates.
         """
@@ -88,59 +86,44 @@ class ProductTracker:
         return data
 
 
-    def scrape_price(self) -> float:
-        """
-        Scrapes Amazon's productpage for current USD pricepoint.
-        """
-        driver = webdriver.Chrome(self.driver_path)
-        driver.get(self.product_link)
-        with open( 
-            "data\\" + self.filename + "_page.html", 
-            "w", 
-            encoding='utf-8'
-        ) as f:
-            f.write(driver.page_source)
-        with open("data\\"+ self.filename + "_page.html","rb") as f:
-            soup=BeautifulSoup(f, 'lxml')
-        return float(
-            soup.find("span", class_="a-offscreen").text.replace("$", "")
-        )
-
-
-    def update_df(self):
+    def update_data(self):
         """
         Scrapes for a new pricepoint and adds it to Dataframe.
         """
-        price_usd = self.scrape_price()
-        new_row = [self.present_date(),price_usd]
+        price_usd = self.scrape_price_usd()
+        new_row = [self.current_date(),price_usd]
         for currency in list(self.thresholds.keys()):
             if currency == 'usd':
                 continue
             else:
-                new_row.append(price_usd * self.coins_per_usd(currency))
+                new_row.append(price_usd * self.coin_per_usd(currency))
         self.df.loc[len(self.df.index)] = new_row
         return
-        
-        
-    def email_notify(self, title, content):
+
+
+    @staticmethod
+    def current_date(include_time: bool = False) -> str:
         """
-        Delivers email notification to user.
+        Returns the present date is DD-MM-YYYY Format. May include 
+        24-hour time as well, formatted as HH:MM.
         """
-        title = "Amazon-Crypto Tracker:" + self.filename + strftime()
-        yagmail.SMTP(self.sender).send(self.recipient, title, content)
-        return
+        date = strftime("%d-%m-%Y", localtime())
+        if include_time:
+            date += " " + strftime("%H:%M", localtime())
+        return date
     
     
-    def coins_per_usd(self, coin_id):
+    @staticmethod
+    def coin_per_usd(self, coin_id):
         """
         Current USD exchange rate for specified cryptocurrency. 
         """
         return 1 / requests.get(
             "https://api.coingecko.com/api/v3/coins/" + coin_id
         ).json()["market_data"]["current_price"]["usd"]
+
     
-    
-    def _to_id(self) -> None:
+    def _cryptokeys_to_id(self) -> None:
         """
         Standardizes every currency key to CoingGecko's ID Format. 
         """
@@ -167,9 +150,9 @@ class ProductTracker:
                 )
     
         self.thresholds.update({term_new: self.thresholds.pop(term_old)})
-        return       
-         
-         
+        return
+
+
     def _validate_connection(self):
         """
         Ensures network connection is valid.
